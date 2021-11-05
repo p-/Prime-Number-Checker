f9280e067148b46fdfad49aa8483af28089f1532
Adam Nativ <admlvntv@gmail.com>
2021-11-05 17:42:07 -0400

    Create tests
    learned how to make JUnit4 tests, created a test for checking prime and non prime numbers

--- src/test/java/me/adamcraftmaster/AppTest.java
+++ src/test/java/me/adamcraftmaster/AppTest.java
@@ -1,20 +1,45 @@
 package me.adamcraftmaster;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
- * Unit test for simple App.
+ * Unit test to ensure if given number is prime
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testIsPrime()
     {
-        assertTrue( true );
+        App tester = new App();
+        //check that prime numbers return valid
+        //System.out.println("Starting Prime test");
+        assertTrue(tester.isPrime(2));
+        assertTrue(tester.isPrime(3));
+        assertTrue(tester.isPrime(5));
+        assertTrue(tester.isPrime(7));
+        assertTrue(tester.isPrime(11));
+        assertTrue(tester.isPrime(13));
+        assertTrue(tester.isPrime(17));
+        assertTrue(tester.isPrime(19));
+    }
+    @Test
+    public void testIsNotPrime()
+    {
+        App tester = new App();
+        //check that non-prime numbers return invalid
+        //System.out.println("Starting Not Prime test");
+        assertFalse(tester.isPrime(4));
+        assertFalse(tester.isPrime(6));
+        assertFalse(tester.isPrime(8));
+        assertFalse(tester.isPrime(9));
+        assertFalse(tester.isPrime(10));
+        assertFalse(tester.isPrime(12));
+        assertFalse(tester.isPrime(14));
+        assertFalse(tester.isPrime(15));
+        assertFalse(tester.isPrime(16));
+        assertFalse(tester.isPrime(18));
+        assertFalse(tester.isPrime(20));
     }
 }
